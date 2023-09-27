e1fe6899af6b68620e9473380166cb2a8d7294db
juniortour <juniortour@qq.com>
2023-09-27 23:24:13 +0800

    feat: /gauge-metric && useGauge

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, useCounter } = require('./prom-client');
+const { register, useCounter, useGauge } = require('./prom-client');
 
 const app = express();
 
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 /*
-2. 新增POST方法接口，接收客户端传来的任意自定义指标
+2. 新增POST方法接口，接收客户端传来的任意**计数类**自定义指标
 
 客户端发送数据用法：
 await fetch('http://localhost:4001/counter-metric', {
@@ -61,6 +61,43 @@ app.post('/counter-metric', function (req, res) {
   useCounter({ name, help, labels });
 
   const message = `/counter-metric name=${name} labels=${JSON.stringify(
+    labels
+  )}`;
+  console.log(message);
+  res.status(200).json({ message });
+});
+
+/*
+2. 新增POST方法接口，接收客户端传来的任意**数值类**自定义指标
+
+客户端发送数据用法：
+await fetch('http://localhost:4001/gauge-metric', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: 'AssetsSize',
+      help: 'AssetsSize data of front end',
+      labels: {
+        timestamp: Date.now()
+      },
+      value: 996,
+    }),
+})
+*/
+app.post('/gauge-metric', function (req, res) {
+  const { name, help, labels = {}, value } = req.body;
+  if (!name) {
+    console.warn(
+      `/gauge-metric WARN: no name req: ${JSON.stringify(req.body, null, 2)}`
+    );
+    return;
+  }
+
+  useGauge({ name, help, labels }, value);
+
+  const message = `/gauge-metric name=${name}, value=${value} labels=${JSON.stringify(
     labels
   )}`;
   console.log(message);
--- src/prom-client.js
+++ src/prom-client.js
@@ -19,7 +19,23 @@ function useCounter({ name, help, labels }) {
   counter.inc(labels, 1);
 }
 
+
+function useGauge({ name, help, labels }, value) {
+  let gauge = register.getSingleMetric(name);
+  if (!gauge) {
+    gauge = new client.Gauge({
+      name,
+      help,
+      registers: [register],
+      labelNames: Object.keys(labels),
+    });
+  }
+
+  gauge.set(labels, value);
+}
+
 module.exports = {
   useCounter,
+  useGauge,
   register,
 };
